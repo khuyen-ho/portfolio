83fce0cf6f3f2b18d9687809d91d3234a8b6cea9
Ho Lam Bao Khuyen <63384365+HoLamBaoKhuyen@users.noreply.github.com>
2024-07-03 02:11:18 +0700

    update cv

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+import pdf from "../../Assets/khuyen-cv.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
+import preview1 from "../../Assets/khuyen-cv1.jpg";
+import preview2 from "../../Assets/khuyen-cv2.jpg";
+
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
@@ -31,10 +34,32 @@ function ResumeNew() {
           </Button>
         </Row>
 
-        <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
+        <Row className="resume" style={{ margin: "auto" }}>
+          {/* <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
+          </Document> */}
+          <Col xs={12} md={6} className="d-flex justify-content-center mb-3">
+            <img
+              src={preview1}
+              alt="cv1"
+              style={{
+                maxWidth: "100%",
+                height: "auto",
+                objectFit: "contain",
+              }}
+            />
+          </Col>
+          <Col xs={12} md={6} className="d-flex justify-content-center mb-3">
+            <img
+              src={preview2}
+              alt="cv2"
+              style={{
+                maxWidth: "100%",
+                height: "auto",
+                objectFit: "contain",
+              }}
+            />
+          </Col>
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
